988aa0699681b1a02ab2352289f5c09469d52716
Deekshitha Gangavarapu <deekshithagangavarapu@gmail.com>
2024-05-15 22:57:06 +0530

    changes done register and service.ts

--- Angular/recipe/src/app/register/register.component.ts
+++ Angular/recipe/src/app/register/register.component.ts
@@ -1,35 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css'] // Note the correct property name "styleUrls" instead of "styleUrl"
 })
-
 export class RegisterComponent implements OnInit {
-  user:any;
-  service: any;
+
+  user: any;
   countries: any;
-  constructor() {
-    this.user= {
+
+  constructor(private service: UserService, private router: Router) {
+    this.user = {
       userName: '',
-      gender:'',
-      country:'',
-      mobileNum:'',
-      emailId:'',
-      password:'',
-    };
+      gender: '',
+      country: '',
+      mobileNum: '',
+      emailId: '',
+      password: ''
+    }
   }
-  ngOnInit(): void {
-    this.service.getAllContries().subscribe((data: any) => { this.countries = data; });
-    
+
+  ngOnInit() {
+    this.service.getAllCountries().subscribe((data: any) => { this.countries = data; });
   }
+
   submit() {
     console.log(this.user);
     this.service.registerUser(this.user).subscribe((data: any) => {
       console.log(data);
     });
+    console.log("Registration successful");
+    this.router.navigate(['home']);
   }
- 
-
 }
