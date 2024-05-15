e782a1ff54e174c6f257fb4d263ca26a07a2fae8
Deekshitha Gangavarapu <deekshithagangavarapu@gmail.com>
2024-05-15 23:46:52 +0530

    home and about is added

--- Angular/recipe/src/app/app.module.ts
+++ Angular/recipe/src/app/app.module.ts
@@ -1,29 +1,49 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { authGuard } from './auth.guard';
-import { LogoutComponent } from './logout/logout.component';
-import { IngrediantsComponent } from './ingrediants/ingrediants.component';
-// import { HomeComponent } from './home/home.component';
-// import { AboutComponent } from './about/about.component';
+import { BrowserModule } from '@angular/platform-browser';
 
-const routes: Routes = [
-  {path:'',            component:LoginComponent},
-  {path:'login',       component:LoginComponent},
-  {path:'register',    component:RegisterComponent},
-  // {path:'home',        component:HomeComponent},
-  // {path:'about',       component:AboutComponent},
-  {path:'ingrediants', component:IngrediantsComponent},
-  {path:'logout',      canActivate:[authGuard], component:LogoutComponent}
-];
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { RegisterComponent } from './register/register.component';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { LogoutComponent } from './logout/logout.component';
+import { NgToastModule } from 'ng-angular-popup';
+import { IngrediantsComponent } from './ingrediants/ingrediants.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NgxCaptchaModule } from 'ngx-captcha';
+
+
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
   declarations: [
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    RegisterComponent,
     LogoutComponent,
-    IngrediantsComponent
-  ]
+    IngrediantsComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule,
+    ReactiveFormsModule,
+    NgToastModule,
+    NgxCaptchaModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppRoutingModule { }
+export class AppModule { }
