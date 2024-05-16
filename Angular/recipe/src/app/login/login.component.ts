e6a25dbbb39bba5036f42ca85d012ed10164e62a
thanniru shruthika <thannirushruthika247@gmail.com>
2024-05-16 19:05:09 +0530

    Captcha page is added to the login

--- Angular/recipe/src/app/login/login.component.ts
+++ Angular/recipe/src/app/login/login.component.ts
@@ -1,4 +1,3 @@
-
 // import { Component } from '@angular/core';
 // import { Router } from '@angular/router';
 // import { NgToastService } from 'ng-angular-popup';
@@ -40,50 +39,53 @@
 
 
 
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgToastService } from 'ng-angular-popup';
-import { NgxCaptchaModule, ReCaptcha2Component } from 'ngx-captcha'; // Correct import
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
-})
-export class LoginComponent {
 
-  @ViewChild('captchaElem') captchaElem!: ReCaptcha2Component; // Initialize property with ! operator
+})
+export class LoginComponent  implements OnInit{
+  protected aFormGroup!: FormGroup;
+  siteKey: string;
 
   emailId: string;
   password: string;
   mobile: any;
   captchaResponse: string;
+ 
 
-  constructor(private toast: NgToastService, private router: Router) {
+  constructor(private toast: NgToastService,private router:Router,private formBuilder:FormBuilder) {
+    this.siteKey = "6LeQPd4pAAAAAOwJFqx3fsIBYiyFSfdQlo79XZop";
     this.emailId = "";
     this.password = "";
     this.mobile = "";
     this.captchaResponse = "";
   }
+  
 
   loginSubmit(loginForm: any) {
-    if (this.captchaElem && this.captchaElem.getResponse()) {
-      console.log(loginForm);
+    console.log(loginForm);
 
-      if (loginForm.emailId == "admin@gmail.com" && loginForm.password == "123") {
-        console.log('Login Success');
-        this.toast.success({ detail: "Success Message", summary: "Login Success", duration: 5000 });
-        this.router.navigate(['home']);
+    if (loginForm.emailId == "admin@gmail.com" && loginForm.password == "123") {
+      console.log('Login Success');
+      this.toast.success({ detail: "Success Message", summary: "Login Success", duration: 5000 })
+      this.router.navigate(['home']);
 
-      } else {
-        console.log("Invalid Credentials");
-        this.toast.error({ detail: "Error Message", summary: "Invalid Credentials", duration: 5000 });
-
-      }
     } else {
-      console.log("Please complete the CAPTCHA");
-      this.toast.error({ detail: "Error Message", summary: "CAPTCHA Validation", duration: 5000 });
+      console.log("Invalid Credentials")
+      this.toast.error({ detail: "Error Message", summary: "Invalid Credentials", duration: 5000 })
+
     }
   }
+  ngOnInit(): void {
+    this.aFormGroup = this.formBuilder.group({
+      recaptcha: ['', Validators.required]
+    });
 }
-
+}
