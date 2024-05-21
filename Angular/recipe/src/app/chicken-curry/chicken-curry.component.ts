c31010af2fe0c85c2ed537ad5f15c6263b390c13
thanniru shruthika <thannirushruthika247@gmail.com>
2024-05-21 22:17:44 +0530

    Changes made in the chicken curry are added

--- Angular/recipe/src/app/chicken-curry/chicken-curry.component.ts
+++ Angular/recipe/src/app/chicken-curry/chicken-curry.component.ts
@@ -2,15 +2,18 @@ import { Component } from '@angular/core';
 import { NgToastService } from 'ng-angular-popup';
 
 @Component({
+
   selector: 'app-chicken-curry',
   templateUrl: './chicken-curry.component.html',
   styleUrl: './chicken-curry.component.css'
 })
 export class ChickenCurryComponent {
-  constructor(private toast: NgToastService) {}
+  constructor(private toast: NgToastService) { }
 
   purchaseIngredients() {
     this.toast.success({ detail: "Success Message", summary: "purchase Success", duration: 5000 })
+
+
   }
 
 }
