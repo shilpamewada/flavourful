f8987c111c6edd43333b1dabd147d13b2f7266b3
Manasa Bhumireddy <bhumireddymanasa@gmail.com>
2024-05-26 22:01:23 +0530

    changes done in recipe component

--- Angular/recipe/src/app/app-routing.module.ts
+++ Angular/recipe/src/app/app-routing.module.ts
@@ -1,23 +1,67 @@
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { LoginComponent } from './login/login.component';
+// import { RegisterComponent } from './register/register.component';
+// import { authGuard } from './auth.guard';
+// import { LogoutComponent } from './logout/logout.component';
+// import { HomeComponent } from './home/home.component';
+// import { AboutComponent } from './about/about.component';
+// import { ChickenCurryComponent } from './chicken-curry/chicken-curry.component';
+// import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+
+// import { ChickenBiryaniComponent } from './chicken-biryani/chicken-biryani.component';
+// import { PulihoraComponent } from './pulihora/pulihora.component';
+// import { ChickenFriedRiceComponent } from './chicken-fried-rice/chicken-fried-rice.component';
+// import { VegBiryaniComponent } from './veg-biryani/veg-biryani.component';
+// import { VegNoodlesComponent } from './veg-noodles/veg-noodles.component';
+// import { MuttonBiryaniComponent } from './mutton-biryani/mutton-biryani.component';
+
+// import { ChickenPakkodaComponent } from './chicken-pakkoda/chicken-pakkoda.component';
+
+// const routes: Routes = [
+//   {path:'',            component:LoginComponent},
+//   {path:'login',       component:LoginComponent},
+//   {path:'register',    component:RegisterComponent},
+//   {path:'home',        component:HomeComponent},
+//   {path:'about',       component:AboutComponent},
+//   {path:'logout',      canActivate:[authGuard], component:LogoutComponent},
+// {path:'chickencurry',component:ChickenCurryComponent},
+// {path:'addrecipe',component:AddRecipeComponent},
+// {path:'chickenpakkoda',component:ChickenPakkodaComponent},
+// {path:'chickenpakkoda',component:ChickenPakkodaComponent},
+// {path:'chickenbiryani',component:ChickenBiryaniComponent},
+// {path:'muttonbiryani',component:MuttonBiryaniComponent},
+// {path:'pulihora',component:PulihoraComponent},
+//  {path:'chickenfriedrice',component:ChickenFriedRiceComponent},
+//  {path:'vegetablebiryani',component:VegBiryaniComponent},
+//  {path:'vegnoodles',component:VegNoodlesComponent}
+// ];                                                                                                    
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { authGuard } from './auth.guard';
 import { LogoutComponent } from './logout/logout.component';
-import { IngrediantsComponent } from './ingrediants/ingrediants.component';
+
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ChickenCurryComponent } from './chicken-curry/chicken-curry.component';
-import { AddRecipeComponent } from './add-recipe/add-recipe.component';
-
+import { ChickenPakkodaComponent } from './chicken-pakkoda/chicken-pakkoda.component';
 import { ChickenBiryaniComponent } from './chicken-biryani/chicken-biryani.component';
+import { MuttonBiryaniComponent } from './mutton-biryani/mutton-biryani.component';
 import { PulihoraComponent } from './pulihora/pulihora.component';
 import { ChickenFriedRiceComponent } from './chicken-fried-rice/chicken-fried-rice.component';
 import { VegBiryaniComponent } from './veg-biryani/veg-biryani.component';
 import { VegNoodlesComponent } from './veg-noodles/veg-noodles.component';
-import { MuttonBiryaniComponent } from './mutton-biryani/mutton-biryani.component';
-
-import { ChickenPakkodaComponent } from './chicken-pakkoda/chicken-pakkoda.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
 
 const routes: Routes = [
   {path:'',            component:LoginComponent},
@@ -25,11 +69,10 @@ const routes: Routes = [
   {path:'register',    component:RegisterComponent},
   {path:'home',        component:HomeComponent},
   {path:'about',       component:AboutComponent},
-  {path:'ingrediants', component:IngrediantsComponent},
+  {path:'addrecipe',   component:AddRecipeComponent},
+  
   {path:'logout',      canActivate:[authGuard], component:LogoutComponent},
 {path:'chickencurry',component:ChickenCurryComponent},
-{path:'addrecipe',component:AddRecipeComponent},
-{path:'chickenpakkoda',component:ChickenPakkodaComponent},
 {path:'chickenpakkoda',component:ChickenPakkodaComponent},
 {path:'chickenbiryani',component:ChickenBiryaniComponent},
 {path:'muttonbiryani',component:MuttonBiryaniComponent},
@@ -37,7 +80,7 @@ const routes: Routes = [
  {path:'chickenfriedrice',component:ChickenFriedRiceComponent},
  {path:'vegetablebiryani',component:VegBiryaniComponent},
  {path:'vegnoodles',component:VegNoodlesComponent}
-];                                                                                                    
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular/recipe/src/app/app.module.ts
+++ Angular/recipe/src/app/app.module.ts
@@ -1,3 +1,63 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { LoginComponent } from './login/login.component';
+// import { HeaderComponent } from './header/header.component';
+// import { RegisterComponent } from './register/register.component';
+
+// import { HttpClientModule } from '@angular/common/http';
+
+// import { RouterModule } from '@angular/router';
+
+// import { LogoutComponent } from './logout/logout.component';
+// import { NgToastModule } from 'ng-angular-popup';
+// import { IngrediantsComponent } from './ingrediants/ingrediants.component';
+// import { HomeComponent } from './home/home.component';
+// import { AboutComponent } from './about/about.component';
+// import {FormGroup, FormsModule,ReactiveFormsModule } from '@angular/forms';
+// import { NgxCaptchaModule } from 'ngx-captcha';
+// import { ChickenCurryComponent } from './chicken-curry/chicken-curry.component';
+// import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+// import { ChickenPakkodaComponent } from './chicken-pakkoda/chicken-pakkoda.component';
+// // import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+
+
+
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     LoginComponent,
+//     HeaderComponent,
+//     RegisterComponent,
+//     LogoutComponent,
+//     IngrediantsComponent,
+//     HomeComponent,
+//     AboutComponent,
+//     ChickenCurryComponent,
+//     AddRecipeComponent,
+//     ChickenPakkodaComponent,
+
+//     // ForgotPasswordComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     FormsModule,
+//     HttpClientModule,
+//      RouterModule,
+//     NgToastModule,
+//     NgxCaptchaModule,
+//     ReactiveFormsModule
+    
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -13,7 +73,7 @@ import { RouterModule } from '@angular/router';
 
 import { LogoutComponent } from './logout/logout.component';
 import { NgToastModule } from 'ng-angular-popup';
-import { IngrediantsComponent } from './ingrediants/ingrediants.component';
+
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import {FormGroup, FormsModule,ReactiveFormsModule } from '@angular/forms';
@@ -21,6 +81,12 @@ import { NgxCaptchaModule } from 'ngx-captcha';
 import { ChickenCurryComponent } from './chicken-curry/chicken-curry.component';
 import { AddRecipeComponent } from './add-recipe/add-recipe.component';
 import { ChickenPakkodaComponent } from './chicken-pakkoda/chicken-pakkoda.component';
+import { ChickenBiryaniComponent } from './chicken-biryani/chicken-biryani.component';
+import { MuttonBiryaniComponent } from './mutton-biryani/mutton-biryani.component';
+import { PulihoraComponent } from './pulihora/pulihora.component';
+import { ChickenFriedRiceComponent } from './chicken-fried-rice/chicken-fried-rice.component';
+import { VegBiryaniComponent } from './veg-biryani/veg-biryani.component';
+import { VegNoodlesComponent } from './veg-noodles/veg-noodles.component';
 // import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 
@@ -33,12 +99,18 @@ import { ChickenPakkodaComponent } from './chicken-pakkoda/chicken-pakkoda.compo
     HeaderComponent,
     RegisterComponent,
     LogoutComponent,
-    IngrediantsComponent,
+   
     HomeComponent,
     AboutComponent,
-    ChickenCurryComponent,
+    ChickenCurryComponent,    
     AddRecipeComponent,
-    ChickenPakkodaComponent
+    ChickenPakkodaComponent,
+    ChickenBiryaniComponent,
+    MuttonBiryaniComponent,
+    PulihoraComponent,
+    ChickenFriedRiceComponent,
+    VegBiryaniComponent,
+    VegNoodlesComponent
     // ForgotPasswordComponent
   ],
   imports: [
